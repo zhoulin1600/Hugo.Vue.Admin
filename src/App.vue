044c8b219384b8d0030028cc74285ab3bf0ec1cf
zhoulin1600 <45185908@qq.com>
2020-11-10 10:33:38 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,19 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
+
+    <div>
+      <a-button type="primary"> Primary </a-button>
+      <a-button>Default</a-button>
+      <a-button type="dashed"> Dashed </a-button>
+      <a-button type="danger"> Danger </a-button>
+      <a-config-provider :auto-insert-space-in-button="false">
+        <a-button type="primary"> 按钮 </a-button>
+      </a-config-provider>
+      <a-button type="primary"> 按钮 </a-button>
+      <a-button type="link"> Link </a-button>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// ant-design-vue
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.use(Antd)
+
 Vue.config.productionTip = false
 
 new Vue({
